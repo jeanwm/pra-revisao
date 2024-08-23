a6a87483fff85b5e5b2ced1748149a0be8d16f6f
Jean Ernani Witt Meier <71325720+jeanwm@users.noreply.github.com>
2024-08-22 21:23:57 -0300

    Add files via upload

--- praRevisao2.c
+++ praRevisao2.c
@@ -9,21 +9,14 @@ int main() {
 	int v[] = { 1, 2, 3, 4, 5 };
 	int vRes[5];
 	int i = 0, j = 0;
-	int acumulado = 0;
+	int acumulado = 1;
+		
+	for (i = 0; i < n; i++) {
+		acumulado = v[i] * acumulado; 
+	}
 	
 	for (i = 0; i < n; i++) {
-		acumulado = 1;
-		
-		for (j = 0; j < n; j++) {
-			if (i != j) {
-				printf("%d %d, ", v[j], acumulado);
-				
-				acumulado = v[j] * acumulado; 
-			}
-		}
-		printf("\n");
-		
-		vRes[i] = acumulado;
+		vRes[i] = acumulado / v[i]; 
 	}
 	
 	for (i = 0; i < n; i++) {
